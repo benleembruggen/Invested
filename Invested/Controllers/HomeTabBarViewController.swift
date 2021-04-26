561a2d6088e2708c1357b7e92619af591c3fcdda
benleembruggen <benleembruggen@tpg.com.au>
2021-04-26 19:12:45 +1000

    Created new views and linked after successful auth

---
+++ Invested/Controllers/HomeTabBarViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeTabBarViewController.swift
+//  Invested
+//
+//  Created by Ben Leembruggen on 26/4/21.
+//
+
+import UIKit
+
+class HomeTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
