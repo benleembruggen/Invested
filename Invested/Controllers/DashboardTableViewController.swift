92f3d318a80cf6611571cf9882837ba5cf995b65
benleembruggen <benleembruggen@tpg.com.au>
2021-04-28 16:32:17 +1000

    Set up table view controller with hard coded data

--- Invested/Controllers/DashboardTableViewController.swift
+++ Invested/Controllers/DashboardTableViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class DashboardTableViewController: UITableViewController {
 
+    let SECTION_GRAPH = 0
+    let SECTION_STOCKS = 1
+    let CELL_GRAPH = "graphCell"
+    let CELL_STOCK = "stockCell"
+    
+    var stockArray = ["APPL", "TSLA", "GME"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -22,59 +29,67 @@ class DashboardTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 2
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        switch section {
+            case SECTION_GRAPH:
+                return 0
+            case SECTION_STOCKS:
+                return stockArray.count
+            default:
+                return 0
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        // return the header title for each section in the table view
+        var text = ""
+        
+        switch section {
+            case 0:
+                text = "Dashboard"
+            case 1:
+                text = "Current Positions"
+            default:
+                text = ""
+        }
+        return text
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+        if indexPath.section == SECTION_STOCKS {
+            let cell = tableView.dequeueReusableCell(withIdentifier: CELL_STOCK, for: indexPath)
+            cell.textLabel?.text = stockArray[indexPath.row]
+            return cell
+        } else {
+            return tableView.dequeueReusableCell(withIdentifier: CELL_STOCK, for: indexPath)
+        }
     }
-    */
+    
 
-    /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+        if indexPath.section == SECTION_STOCKS {
+            return true
+        }
+        return false
     }
-    */
+    
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+            stockArray.remove(at: indexPath.row)
+            tableView.reloadSections([SECTION_STOCKS], with: .automatic)
         }    
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
+    
 
     /*
     // MARK: - Navigation
